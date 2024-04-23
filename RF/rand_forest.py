60903ab97d023423cd3e28f84133f2049e84c497
Eduardo MCL <eduardo.m.cabeza.l@gmail.com>
2024-04-23 20:21:18 +0200

    #9 Actualizar modelo
    Se utilizan los conjuntos de datos balanceados. Se elimina las líneas para eliminar características adicionales (se eliminan en el procesado). Se añaden parámetros al modelo para su ajuste. Se añade la medición del tiempo de entrenamiento.

--- RF/rand_forest.py
+++ RF/rand_forest.py
@@ -1,5 +1,5 @@
 
-import os, pickle
+import os, pickle, time
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
@@ -7,8 +7,8 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, classification_report
 
 # Carga de datos
-train_data = pd.read_csv('..' + os.sep + 'data' + os.sep + 'train_network.csv')
-test_data = pd.read_csv('..' + os.sep + 'data' + os.sep + 'test_network.csv')
+train_data = pd.read_csv('..' + os.sep + 'data' + os.sep + 'balanced_train.csv')
+test_data = pd.read_csv('..' + os.sep + 'data' + os.sep + 'balanced_test.csv')
 y_train = train_data.pop('label')
 y_test = test_data.pop('label')
 
@@ -18,16 +18,14 @@ class_1 = sum(y_train==1)
 
 class_w = {0: 1/(2*class_0), 1: 1/(2*class_1)}
 
-# Eliminar algunas caracteristicas más. Esto se realizará en posteriormente en el preprocesado de los datasets
-cols_to_drop = [col for col in train_data.columns if col.startswith('dst_ip_') or col.startswith('src_ip_') or col in ['src_port', 'dst_port', 'ts']]
-# Eliminar las columnas
-train_data = train_data.drop(columns=cols_to_drop)
-test_data = test_data.drop(columns=cols_to_drop)
 # Crear el modelo
-rfc = RandomForestClassifier(class_weight=class_w, max_depth=10)
+rfc = RandomForestClassifier(class_weight=class_w, max_depth=25, n_estimators=100, random_state=24)
+#Obtener tiempo incial
+t_ini = time.time()
 # Entrenar el modelo
 rfc.fit(train_data, y_train)
-
+# Obtener tiempo de entrenamiento
+t_train = time.time() - t_ini
 # Predecir con el conjunto de test
 predictions = rfc.predict(test_data)
 predictions = predictions.astype(int)
@@ -42,6 +40,10 @@ plt.show()
 report = classification_report(y_test, predictions)
 print("Reporte de clasificación:")
 print(report)
+
+#Imprimir tiempo de entrenamiento
+print()
+print(f'Tiempo de entrenamiento: {t_train:.2f} segundos')
 
 '''
 # Guardar el modelo entrenado
