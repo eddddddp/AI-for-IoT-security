9beaaf9557fef39a8b21188aa6708ad2c339cb12
Eduardo MCL <eduardo.m.cabeza.l@gmail.com>
2024-04-29 17:26:20 +0200

    #11 Se añade modulo model_utils.py
    Se añade un modulo para las funciones de utilidad a usar en la creación y entrenamiento de los modelos. Inicialmente contiene una función para la impresión de métricas y matriz de confusión.

---
+++ Models/model_utils.py
@@ -0,0 +1,13 @@
+from sklearn.metrics import confusion_matrix, accuracy_score, classification_report, ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
+
+
+def metrics(objetivo, prediction):
+    cm = confusion_matrix(objetivo, prediction)
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm)
+    disp.plot()
+    plt.show()
+    score = accuracy_score(objetivo, prediction)
+    reporte = classification_report(objetivo, prediction)
+    metricas = [cm, score, reporte]
+    return(metricas)
