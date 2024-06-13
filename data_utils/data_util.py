4d7e63c8989fa6e9bf14cc756d562e4e1048ea43
Eduardo MCL <eduardo.m.cabeza.l@gmail.com>
2024-06-13 13:13:30 +0200

    #20 Se limpia y comenta el código de data_util.py
    Se limpia el código y se añaden comentarios al módulo data_utils.py.

--- data_utils/data_util.py
+++ data_utils/data_util.py
@@ -1,22 +1,16 @@
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import MinMaxScaler, OneHotEncoder
 from sklearn.model_selection import train_test_split
 import pandas as pd
-import ipaddress
 
-# Función para convertir la dirección IP a una lista de 16 octetos
-def ipv4_to_octets(ip):
-    try:
-        ip = ipaddress.ip_address(ip)
-        if ip.version == 4:
-            # Devolver los octetos
-            return list(ip.packed)        
-    except ValueError:
-        print('Se ha encontrado una IP no válida en el dataset')
-
-# Función que realiza el one-hot encoding a las características del dataframe pasado por parámetro    
+'''
+Función que dado un conjunto de datos que solo contiene atributos categóricos
+los codifica como valores numéricos mediante la técnica de one-hot encoding.
+Parameters: Dataset con características a codificar.
+Return: Dataset con características codificadas.
+'''
 def one_hot_encode(data):
-    # Obtener encoder. Se eliminan las columnas “dummy” con la opción drop
-    encoder = OneHotEncoder(sparse_output=False, drop='first')
+    # Obtener encoder
+    encoder = OneHotEncoder(sparse_output=False)
     # Codificar las columnas de data
     data_encoded = encoder.fit_transform(data)
     # Convertir a dataframe de pandas 
@@ -26,6 +20,33 @@ def one_hot_encode(data):
     # Devolver el dataframe
     return data_encoded
 
+'''
+Función que dado un conjunto de datos, una fracción en tanto por uno de los datos
+a usar en entrenamiento, y una columna de estatificación, devuelve las particiones de 
+entrenamiento y validación en las proporciones indicadas de forma estratificada por 
+la característica seleccionada.
+Parameters: Dataset, fracción de particion de entrenamiento, característica de estratificación.
+Return: Partición de entrenamiento y partición de test o validación.
+'''
 def split_data(data,train_percent,stratify_col):
+    # Obtener particiones de entrenamiento y validación o test
     train_data, test_data = train_test_split(data,train_size=train_percent,stratify=stratify_col)
     return train_data, test_data
+
+'''
+Función que dadas las particiones de entrenamiento y test, realiza la normalización
+de los valores de ambas particiones.
+Parameters: Partición de entrenamiento, partición de validación.
+Return: Partición de entrenamiento normalizada, partición de validación normalizada.
+'''
+def normalizeData(traindata, testdata):
+    # Instanciar scaler
+    scaler = MinMaxScaler()   
+    # Normalizar partición de entrenamiento
+    train_scaled = scaler.fit_transform(traindata)
+    # Normalizar partición de validación
+    test_scaled = scaler.transform(testdata)
+    # Convertir particiones a DataFrame de pandas
+    train_scaled = pd.DataFrame(train_scaled, columns=traindata.columns)
+    test_scaled = pd.DataFrame(train_scaled, columns=testdata.columns)
+    return train_scaled, test_scaled
