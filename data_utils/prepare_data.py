8814c3619b173fc5544652ee90afd89968bb62f7
Eduardo MCL <eduardo.m.cabeza.l@gmail.com>
2024-06-13 13:12:10 +0200

    #20 Limpiar y comentar prepare_data.py
    Se limpia el código y se añaden comentarios al script prepare_data.py.

--- data_utils/prepare_data.py
+++ data_utils/prepare_data.py
@@ -1,13 +1,20 @@
 import pandas as pd
-from data_util import one_hot_encode
 import sys
+from data_util import one_hot_encode
 
-from sklearn.preprocessing import LabelEncoder
+'''
+Script para preparar el conjunto de datos. Recibe en línea de comandos la
+ruta de origen de los datos y el destino donde se guardará el conjunto preparado.
+Se seleccionan las características que se mantienen, se limpian los registros anómalos, 
+se codifican mediante one-hot encoding las características categóricas y se eliminan
+los registros que contienen nulos.
+Ejemplo de uso:
+py prepare_date.py "..\data\my_dataset.csv" "..\data\my_prepared_dataset.csv"
+'''
 # Recoger argumentos en línea de comandos
-# Esperado: <input_filename> <data_output_filename> <flag si usa type o label>. Si flag true se usa type en cualquier otro caso label
-#  Ejemplo: python prepare_ipv4_data.py "..\data\10_per_data.csv" "..\data\10_processed.csv" true/false
 args = sys.argv
 
+# Comprobar el número de parámetros recibidos
 if len(args) != 4:
     print(f'Se esperaban 3 argumentos pero se obtuvieron {len(args)-1}')
     print('Se esperaba <src data> <dst data> <target_flag>')
@@ -20,32 +27,19 @@ data = data[data['src_bytes'] != '0.0.0.0']
 
 # Obtener características y separar numéricas y texto o str
 data_str = data[['proto','service','conn_state']]
+
 # Convertir a float las numéricas
 data_num = data[['dst_bytes','dst_ip_bytes','missed_bytes','dst_pkts','duration','src_bytes','src_ip_bytes','src_pkts']].astype(float)
 
-#data_str = data_str.drop('uid',axis=1)
-if args[3] == 'true':
-    # Crear el codificador
-    le = LabelEncoder()
-    targets = data[['type']]
-    # Ajustar y transformar los datos
-    targets['type_cod'] = le.fit_transform(targets['type']).astype(float)
+# Obtener etiquetas o targets 
+targets = data[['label']].astype(float)
 
-    # Obtener correspondencia entre las clases y los valores asignados
-    print(dict(zip(le.classes_, le.transform(le.classes_))))
-    
-else:   
-    targets = data[['label']].astype(float)
 # Codificar data_str
 data_encoded = one_hot_encode(data_str)
+
 # Reiniciar los índices de las filas
 data_num = data_num.reset_index(drop=True)
 data_encoded = data_encoded.reset_index(drop=True)
-
-'''
-# Normalizar data_num
-data_num = pd.DataFrame(normalize(data_num.values),columns=data_num.columns)
-'''
 
 # Reconstruir el dataset
 full_data = pd.concat([targets,data_num,data_encoded], axis=1)
@@ -53,5 +47,5 @@ full_data = pd.concat([targets,data_num,data_encoded], axis=1)
 # Eliminar filas con NaN
 full_data.dropna(subset=full_data.columns, inplace=True)
 
-# Guardar csv procesado
+# Guardar csv preparado
 full_data.to_csv(args[2],index=False)
