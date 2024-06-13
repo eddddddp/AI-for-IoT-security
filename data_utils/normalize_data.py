04414ec46b8b5f394cd6036014e67936a552cff6
Eduardo MCL <eduardo.m.cabeza.l@gmail.com>
2024-06-13 13:11:41 +0200

    #20 Limpiar y comentar normalize_data.py
    Se limpia el código y se añaden comentarios al script normalize_data.py.

--- data_utils/normalize_data.py
+++ data_utils/normalize_data.py
@@ -1,41 +1,26 @@
 import pandas as pd
-from sklearn.preprocessing import MinMaxScaler, StandardScaler
 import sys
-
-def normalizeData(traindata, testdata):
-    scaler = MinMaxScaler()   
-    train_scaled = scaler.fit_transform(traindata)
-    test_scaled = scaler.transform(testdata)
-    train_scaled = pd.DataFrame(train_scaled, columns=traindata.columns)
-    test_scaled = pd.DataFrame(train_scaled, columns=testdata.columns)
-    return train_scaled, test_scaled
-
-def standarizeData(traindata, testdata):
-    scaler = StandardScaler()   
-    train_scaled = scaler.fit_transform(traindata)
-    test_scaled = scaler.transform(testdata)
-    train_scaled = pd.DataFrame(train_scaled, columns=traindata.columns)
-    test_scaled = pd.DataFrame(train_scaled, columns=testdata.columns)
-    return train_scaled, test_scaled
-
+from data_util import normalizeData
+'''
+Script destinado a la normalización de los valores de las particiones
+de entrenamiento y validación o test en formato CSV. Se pasan las rutas de origen y
+destino como parámetros en línea de comandos.
+Ejemplo de uso:
+py normalize_data.py "..\data\my_train_set.csv" "..\data\my_test_set.csv" "..\data\my_train_set_normalized.csv" "..\data\my_test_set_normalized.csv"
+'''
 # Obtener argumentos
 args = sys.argv
-if len(args) != 6:
-    print(f'Número de argumentos incorrecto, se esperaban 5 pero se obtuvieron {len(args)-1}.')
-    print('Se espera <src train_data> <src test_data> <dst train_norm_data> <dst test_norm_data> <normalize/standarize>')
+if len(args) != 5:
+    print(f'Número de argumentos incorrecto, se esperaban 4 pero se obtuvieron {len(args)-1}.')
+    print('Se espera <src train_data> <src test_data> <dst train_norm_data> <dst test_norm_data>')
     exit(-1)
 
 # Cargar conjuntos de datos
 train_data = pd.read_csv(args[1])
 test_data = pd.read_csv(args[2])
 
-if args[5] == 'normalize':
-    train_data, test_data = normalizeData(train_data, test_data)
-elif args[5] == 'standarize':
-        train_data, test_data = standarizeData(train_data, test_data)
-else:
-     print(f'Parámetro de normalización incorrecto, se esperaba "normalize" o "standarize", pero se obtuvo {args[5]}')
-     exit(-2)
+# Normalizar valores de las particiones
+train_data, test_data = normalizeData(train_data, test_data)
 
 train_data.to_csv(args[3])
 test_data.to_csv(args[4])
