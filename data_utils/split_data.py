bb15b3f38d6f864db8568c9dada2d4a20612682a
Eduardo MCL <eduardo.m.cabeza.l@gmail.com>
2024-06-13 13:12:42 +0200

    #20 Limpiar y comentar split_data.py
    Se limpia el código y se añaden comentarios al script split_data.py.

--- data_utils/split_data.py
+++ data_utils/split_data.py
@@ -1,15 +1,33 @@
 import pandas as pd
 import sys
 from data_util import split_data
-# Ejemplo de uso: python split_data.py "..\data\data.csv" 0.8 label "\data\train_data.csv" ["..\data\test_data.csv"]
+'''
+Script para dividir el conjunto de datos en particiones de entrenamiento y validación o test.
+Recibe en línea de comandos la ruta de los datos de origen, la fracción en tanto por uno
+para entrenamiento, la característica de estratificación, el destino de la partición de 
+entrenamiento y, opcionalmente, el destino de la particion de validación o test. Este script
+también puede ser usado para obtener un subconjunto aleatorio de un conjunto de datos, manteniendo
+la proporción entre las clases.
+Ejemplos de uso:
+py split_data.py "..\data\my_data.csv" 0.8 label "..\data\my_train_data.csv" "..\data\my_test_data"
+py split_data.py "..\data\my_data.csv" 0.5 label "..\data\my_half_data.csv" 
+'''
+
 # Recoger argumentos en línea de comandos
-# Se espera el fichero a dividir, porcentaje de entrenamiento, columna de objetivos y los dos ficheros, train y test
 args = sys.argv
+
+# Comprobar el número de argumentos
+if len(args) != 5 and len(args) != 6:
+    print(f'Número de argumentos incorrecto, se esperaban 4 o 5 pero se obtuvieron {len(args)-1}')
+    print('Se espera <src data> <train_size> <stratify col> <dst train_data> [<dst test_data>]')
+    exit(-1)
 
 # Cargar dataset
 data = pd.read_csv(args[1], low_memory=False)
-# Particionar el dataset
+
+# Crear particiones
 train_data, test_data = split_data(data,float(args[2]),data[args[3]])
+
 # Guardar los datasets
 # Si se pasan 5 argumentos se guarda solo la partición declarada como de entrenamiento
 if len(args) == 6:     
